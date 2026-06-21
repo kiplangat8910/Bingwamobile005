30959d945c38992a3219002a4547e0c3521e8505
threadsautposters-pixel <threadsautposters-pixel@users.noreply.github.com>
2026-06-21 08:58:47 +0000

    chore: bump version to 3.0.82

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 111
-        versionName = "3.0.81"
+        versionCode = 112
+        versionName = "3.0.82"
     }
 
     signingConfigs {
