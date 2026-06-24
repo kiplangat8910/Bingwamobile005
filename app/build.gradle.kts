13aee2e3c39e6164ae037d4cde8e9752df3d34da
threadsautposters-pixel <threadsautposters-pixel@users.noreply.github.com>
2026-06-24 15:42:19 +0000

    chore: release v3.1.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 153
-        versionName = "3.1.23"
+        versionCode = 154
+        versionName = "3.1.24"
     }
 
     signingConfigs {
