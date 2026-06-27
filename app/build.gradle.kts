b331bf2bc943873066bddffe87fb92504e9c6b0f
threadsautposters-pixel <threadsautposters-pixel@users.noreply.github.com>
2026-06-27 11:05:16 +0000

    Bump release to v3.1.45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 174
-        versionName = "3.1.44"
+        versionCode = 175
+        versionName = "3.1.45"
     }
 
     signingConfigs {
