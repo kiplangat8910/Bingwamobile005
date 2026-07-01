fc86d95749b49c2cd2aaa0b4091b70e84d1ccec5
kiplangat8910 <kiplangat8910@users.noreply.github.com>
2026-07-01 15:15:03 +0000

    Release v3.1.77

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 206
-        versionName = "3.1.76"
+        versionCode = 207
+        versionName = "3.1.77"
     }
 
     signingConfigs {
