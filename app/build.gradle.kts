96fec434ebf12d0e66f1736049d10952b42a764b
kiplangat8910 <kiplangat8910@users.noreply.github.com>
2026-07-01 05:03:10 +0000

    chore: release 3.1.68

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 197
-        versionName = "3.1.67"
+        versionCode = 198
+        versionName = "3.1.68"
     }
 
     signingConfigs {
