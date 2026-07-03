90589a10e458c5720c4572c81ef387a29af642eb
kiplangat8910 <kiplangat8910@users.noreply.github.com>
2026-07-03 07:12:07 +0000

    chore: bump version to 3.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 229
-        versionName = "3.2.0"
+        versionCode = 230
+        versionName = "3.2.1"
     }
 
     signingConfigs {
