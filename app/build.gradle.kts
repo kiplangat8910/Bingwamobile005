a593538643c5446e3491f216d1f8139c7c029c2e
kiplangat8910 <kiplangat8910@users.noreply.github.com>
2026-07-22 07:39:34 +0000

    release: bump 3.3.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 35
-        versionCode = 350
-        versionName = "3.3.21"
+        versionCode = 351
+        versionName = "3.3.22"
     }
 
     signingConfigs {
