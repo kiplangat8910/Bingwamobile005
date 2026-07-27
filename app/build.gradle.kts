45a44ecc4c01d25dccae67b7f6aa7c233970fa2c
kiplangat8910 <kiplangat8910@users.noreply.github.com>
2026-07-27 11:26:41 +0000

    Release v3.3.52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 35
-        versionCode = 380
-        versionName = "3.3.51"
+        versionCode = 381
+        versionName = "3.3.52"
     }
 
     signingConfigs {
