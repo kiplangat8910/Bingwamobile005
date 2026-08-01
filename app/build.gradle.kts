4f5d219c4c8b8b8b41d891eb01ef30b365de4ceb
kiplangat8910 <kiplangat8910@gmail.com>
2026-08-01 12:29:02 +0000

    chore: bump version to 3.3.96

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 35
-        versionCode = 403
-        versionName = "3.3.95"
+        versionCode = 404
+        versionName = "3.3.96"
     }
 
     signingConfigs {
