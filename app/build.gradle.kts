3278055baf9395f334bff6ccc382a31682da072f
kiplangat8910 <kiplangat8910@gmail.com>
2026-08-01 23:53:00 +0000

    chore: bump version to 3.4.07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 35
-        versionCode = 414
-        versionName = "3.4.06"
+        versionCode = 415
+        versionName = "3.4.07"
     }
 
     signingConfigs {
