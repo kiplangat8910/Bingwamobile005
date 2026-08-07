3db34d5e658797c6c0badbae8cefa408140e5c09
kiplangat8910 <kiplangat8910@gmail.com>
2026-08-07 01:54:25 +0000

    chore: bump adminhub version to 1.0.4

--- adminhub/build.gradle.kts
+++ adminhub/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bingwa.adminhub"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     signingConfigs {
